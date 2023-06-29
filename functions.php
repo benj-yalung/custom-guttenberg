2f2c8e1cdeafd6fa30e5ab2259e3894b07c0ff31
benj-yalung <benjamin.yalung2020@gmail.com>
2023-06-29 16:10:58 +0800

    Fixed the font issue

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function my_theme_enqueue_styles() {
-	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parenthandle' ), '1.0.0' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', ['twentytwentytwo-style'], '2.0.0' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
